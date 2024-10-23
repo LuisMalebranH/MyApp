c8577281f1fa9e6225d86c306db0ca9919d1620d
jucarlos10 <ju.valenciay@duocuc.cl>
2024-10-23 13:22:15 -0300

    confi para ocupar api nativa

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -95,3 +95,27 @@ export class DataService {
     this.images.push(image);
   }
 } */
+
+/*
+import { Camera, CameraResultType,CameraSource } from '@capacitor/camera';
+
+ async takePicture(PromptLabelHeader: String) {
+ return await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.dataUrl,
+    source:CameraSource.prompt,
+    PromptLabelHeader
+    PromptLabelPhoto
+  });
+
+  // image.webPath will contain a path that can be set as an image src.
+  // You can access the original file using image.path, which can be
+  // passed to the Filesystem API to read the raw data of the image,
+  // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+  var imageUrl = image.webPath;
+
+  // Can be set to the src of an image now
+  imageElement.src = imageUrl;
+};
+*/
